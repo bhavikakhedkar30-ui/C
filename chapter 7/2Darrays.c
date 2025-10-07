8377a5855a512ae8c044275a4ddadaeced366a3a
unknown <bhavikakhedkar30@gmail.com>
2025-10-07 23:09:29 +0530

    Arrays

---
+++ chapter 7/2Darrays.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+//syntax of array//
+
+    int marks[2][3];
+     marks[0][0] = 90;
+     marks[0][1] = 85;
+     marks[1][2] = 90;
+
+     marks[1][0] = 89;
+     marks[1][1] = 69;
+     marks[1][2] = 47;
+
+    printf("%d", marks[0][0] );
+    
+
+
+}
