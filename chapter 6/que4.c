cf3b830e5c4332c0494dd0b084354a6185a917ae
unknown <bhavikakhedkar30@gmail.com>
2025-10-22 11:46:07 +0530

    C codes

---
+++ chapter 6/que4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main(){
+    int a = 10;
+    int *p = &a;
+    printf("value at p is : %d",&p);
+
+    //& -> gives me address 
+    // * -> gives me value 
+ }
+
+    
+
+
+
+
+ 
+
